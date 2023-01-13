6d13136257f68f6f3a61c09ae5d5847e039cf7ad
xJaSzyy <novosel.stepan@gmail.com>
2023-01-13 12:11:18 +0700

    g

---
+++ GoldenSection/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace GoldenSection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Accuracy = new System.Windows.Forms.NumericUpDown();
+            this.ResultY = new System.Windows.Forms.TextBox();
+            this.Formula = new System.Windows.Forms.TextBox();
+            this.IntTo = new System.Windows.Forms.TextBox();
+            this.IntFrom = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.построитьГрафикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ResultX = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.Accuracy)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Accuracy
+            // 
+            this.Accuracy.DecimalPlaces = 2;
+            this.Accuracy.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.Accuracy.Location = new System.Drawing.Point(147, 402);
+            this.Accuracy.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            131072});
+            this.Accuracy.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.Accuracy.Name = "Accuracy";
+            this.Accuracy.Size = new System.Drawing.Size(100, 20);
+            this.Accuracy.TabIndex = 19;
+            this.Accuracy.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            // 
+            // ResultY
+            // 
+            this.ResultY.Location = new System.Drawing.Point(177, 428);
+            this.ResultY.Name = "ResultY";
+            this.ResultY.ReadOnly = true;
+            this.ResultY.Size = new System.Drawing.Size(70, 20);
+            this.ResultY.TabIndex = 18;
+            // 
+            // Formula
+            // 
+            this.Formula.Location = new System.Drawing.Point(147, 376);
+            this.Formula.Name = "Formula";
+            this.Formula.Size = new System.Drawing.Size(100, 20);
+            this.Formula.TabIndex = 17;
+            // 
+            // IntTo
+            // 
+            this.IntTo.Location = new System.Drawing.Point(183, 350);
+            this.IntTo.Name = "IntTo";
+            this.IntTo.Size = new System.Drawing.Size(64, 20);
+            this.IntTo.TabIndex = 16;
+            // 
+            // IntFrom
+            // 
+            this.IntFrom.Location = new System.Drawing.Point(88, 350);
+            this.IntFrom.Name = "IntFrom";
+            this.IntFrom.Size = new System.Drawing.Size(64, 20);
+            this.IntFrom.TabIndex = 15;
+            this.IntFrom.Tag = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.построитьГрафикToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 20;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // построитьГрафикToolStripMenuItem
+            // 
+            this.построитьГрафикToolStripMenuItem.Name = "построитьГрафикToolStripMenuItem";
+            this.построитьГрафикToolStripMenuItem.Size = new System.Drawing.Size(121, 20);
+            this.построитьГрафикToolStripMenuItem.Text = "Построить график";
+            this.построитьГрафикToolStripMenuItem.Click += new System.EventHandler(this.построитьГрафикToolStripMenuItem_Click_1);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click_1);
+            // 
+            // ResultX
+            // 
+            this.ResultX.Location = new System.Drawing.Point(89, 428);
+            this.ResultX.Name = "ResultX";
+            this.ResultX.ReadOnly = true;
+            this.ResultX.Size = new System.Drawing.Size(70, 20);
+            this.ResultX.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 404);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Точность";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Формула";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 431);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Минимум";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "до";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Интервал от";
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Location = new System.Drawing.Point(253, 27);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Blue;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.Red;
+            series2.MarkerBorderWidth = 10;
+            series2.MarkerColor = System.Drawing.Color.Red;
+            series2.MarkerSize = 15;
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Cross;
+            series2.Name = "Series2";
+            this.chart.Series.Add(series1);
+            this.chart.Series.Add(series2);
+            this.chart.Size = new System.Drawing.Size(535, 411);
+            this.chart.TabIndex = 27;
+            this.chart.Text = "chart1";
+            this.chart.Click += new System.EventHandler(this.chart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.Accuracy);
+            this.Controls.Add(this.ResultY);
+            this.Controls.Add(this.Formula);
+            this.Controls.Add(this.IntTo);
+            this.Controls.Add(this.IntFrom);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.ResultX);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Accuracy)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown Accuracy;
+        private System.Windows.Forms.TextBox ResultY;
+        private System.Windows.Forms.TextBox Formula;
+        private System.Windows.Forms.TextBox IntTo;
+        private System.Windows.Forms.TextBox IntFrom;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem построитьГрафикToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.TextBox ResultX;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
---
+++ GoldenSection/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using org.mariuszgromada.math.mxparser;
+using static System.Collections.Specialized.BitVector32;
+
+namespace GoldenSection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void построитьГрафикToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (Check())
+            {
+                chart.Series[0].Points.Clear();
+                chart.Series[1].Points.Clear();
+                chart.ChartAreas[0].AxisX.LabelStyle.Format = "0.00";
+                chart.ChartAreas[0].AxisY.LabelStyle.Format = "0.00";
+                double a = Convert.ToDouble(IntFrom.Text);
+                double b = Convert.ToDouble(IntTo.Text);
+                double eps = Convert.ToDouble(Accuracy.Text);
+                double h = 0.1;
+                double y;
+
+
+                Argument argx = new Argument($"x = {a}");
+                double x = a;
+                while (x <= b)
+                {
+                    Expression expy = new Expression(Formula.Text, argx);
+                    y = expy.calculate();
+                    chart.Series[0].Points.AddXY(x, y);
+                    x += h;
+                    argx.setArgumentValue(x);
+                GSS(a, b, eps);
+                }
+                //SetBoundaries();
+            }
+        }
+
+        private void очиститьToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            IntFrom.Text = "";
+            IntTo.Text = "";
+            Formula.Text = "";
+            ResultY.Text = "";
+            ResultX.Text = "";
+            chart.Series[0].Points.Clear();
+            chart.Series[1].Points.Clear();
+        }
+
+        private void SetBoundaries()
+        {
+            double xmin = chart.Series[0].Points.FindMinByValue().XValue;
+            double xmax = chart.Series[0].Points.FindMaxByValue().XValue;
+            Argument argMin = new Argument("x", xmin);
+            Argument argMax = new Argument("x", xmax);
+            Expression expMin = new Expression(Formula.Text, argMin);
+            Expression expMax = new Expression(Formula.Text, argMax);
+            double ymin = expMin.calculate();
+            double ymax = expMax.calculate();
+            chart.ChartAreas[0].AxisY.Minimum = ymin - 0.15;
+            chart.ChartAreas[0].AxisY.Maximum = ymax + 0.15;
+        }
+
+        private bool Check()
+        {
+            if (IntFrom.Text == "")
+            {
+                MessageBox.Show("Начальная точка интервала не задана");
+                return false;
+            }
+            else if (IntTo.Text == "")
+            {
+                MessageBox.Show("Конечная точка интервала не задана");
+                return false;
+            }
+            else if (Formula.Text == "")
+            {
+                MessageBox.Show("Формула не задана");
+                return false;
+            }
+            else if (Convert.ToDouble(IntFrom.Text) > Convert.ToDouble(IntTo.Text))
+            {
+                MessageBox.Show("Интервал задан неверно");
+                return false;
+            }
+            return true;
+        }
+
+        private double Function(double value)
+        {
+            Argument x = new Argument("x", value);
+            Expression e = new Expression(Formula.Text, x);
+            double result = e.calculate();
+            return result;
+        }
+
+        private void DisplayExtremes(double a, double eps)
+        {
+            Argument argx = new Argument($"x = {a}");
+            double x = a;
+            ResultX.Text = chart.Series[0].Points.FindMinByValue().XValue.ToString();
+            argx.setArgumentValue(Convert.ToDouble(ResultX.Text));
+            Expression min = new Expression(Formula.Text, argx);
+            double ymin = min.calculate();
+            ResultY.Text = Math.Round(ymin, (int)(eps * 100)).ToString();
+            chart.Series[1].Points.Clear();
+            chart.Series[1].Points.AddXY(Convert.ToDouble(ResultX.Text), Convert.ToDouble(ResultY.Text));
+            ResultX.Text = Math.Round(chart.Series[0].Points.FindMinByValue().XValue, (int)(eps * 100)).ToString();
+        }
+
+        private void GSS(double a, double b, double eps)
+        {
+            if (Check())
+            {
+                double z = (3 - Math.Sqrt(5)) / 2;
+                double x1 = a + z * (b - a), x2 = b - z * (b - a);
+                b /= 2;
+                a /= 2;
+                while (b-a>eps)
+                {
+                    if (Function(x1) <= Function(x2))
+                    {
+                        b = x2;
+                        x2 = x1;
+                        x1 = a + b - x2;
+                    }
+                    else
+                    {
+                        a = x1;
+                        x1 = x2;
+                        x2 = a + b - x1;
+                    }
+                }
+                double x = (a + b) / 2;
+                ResultX.Text = Math.Round(x, (int)(eps * 100)).ToString();
+                ResultY.Text = Math.Round(Function(x), (int)(eps * 100)).ToString();
+                DisplayExtremes(x, eps);
+            }
+        }
+
+        private void IntFrom_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != 45; //только цифры
+        }
+
+        private void IntTo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != 45;//только цифры
+        }
+
+        private void chart_Click(object sender, EventArgs e)
+        {
+            ChartArea CA = chart.ChartAreas[0];  // quick reference
+            CA.AxisX.ScaleView.Zoomable = true;
+            CA.CursorX.AutoScroll = true;
+            CA.CursorX.IsUserSelectionEnabled = true;
+        }
+    }
+}
